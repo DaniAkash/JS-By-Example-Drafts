e4b8846c2f25236b5730834b604b31d5e61b03a1
DaniAkash <s.daniakash@gmail.com>
2017-04-22 20:07:47 +0530

    delete Task

--- Chapter 1 - TodoList/src/scripts.js
+++ Chapter 1 - TodoList/src/scripts.js
@@ -10,6 +10,18 @@ class ToDos {
     this.generateTaskHtml = this.generateTaskHtml.bind(this);
 
     this.loadTasks();
+    document.getElementById('taskList').addEventListener("click", event=> {
+      if(event.target.id === 'deleteTask') {
+        event.preventDefault();
+        this.deleteTask(event.target);
+      }
+    });
+  }
+
+  deleteTask(target) {
+    let taskIndex = target.getAttribute('data-id');
+    this.tasks.splice(taskIndex, 1);
+    this.loadTasks();
   }
 
   generateTaskHtml(task, index) {
@@ -17,13 +29,13 @@ class ToDos {
       <li class="list-group-item checkbox">
         <div class="row">
           <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
-            <label><input type="checkbox" value="" class="" ${task.isComplete?'checked':''}></label>
+            <label><input id="toggleTaskStatus" type="checkbox" value="" class="" ${task.isComplete?'checked':''}></label>
           </div>
           <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
             ${task.task}
           </div>
           <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
-            <a class="" href="/"><i class="delete-icon glyphicon glyphicon-trash"></i></a>
+            <a class="" href="/"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
           </div>
         </div>
       </li>
