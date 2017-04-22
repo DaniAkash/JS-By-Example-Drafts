4203c40852031ae40ac9197b9d9f7849c926fb58
DaniAkash <s.daniakash@gmail.com>
2017-04-22 08:14:27 +0530

    chapter 1 - webpack testrun

---
+++ Chapter1/src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/styles.css';
+
+class home {
+    constructor() {
+        alert('cool');
+    }
+}
+
+new home();
---
+++ Chapter1/webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const PurifyCSSPlugin = require('purifycss-webpack');
+const glob = require('glob');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const isProduction = (process.env.NODE_ENV === 'production');
+const extractCSS = new ExtractTextPlugin('[chunkhash].style.css');
+const pathsToClean = [
+  'dist'
+];
+const cleanOptions = {
+  root: __dirname,
+  verbose: true,
+  dry: false,
+  exclude: [],
+};
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/js/index.js",
+  output: {
+      path: __dirname + "/dist",
+      filename: "[chunkhash].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'es2015']
+          }
+        }
+      }
+    ]
+  },
+  watch: true,
+  plugins: [
+    extractCSS,
+    new webpack.LoaderOptionsPlugin({
+      minimize: isProduction,
+    }),
+    new PurifyCSSPlugin({
+      paths: glob.sync(__dirname + '/src/*.html'),
+      minimize: isProduction,
+    }),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+  ],
+};
+
+if(isProduction) {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
