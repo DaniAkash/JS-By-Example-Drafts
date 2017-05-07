d698cc1c0733a826b044bb4dfb48278e5cc3124d
DaniAkash <s.daniakash@gmail.com>
2017-05-07 21:26:52 +0530

    Create meme with responsive canvas

--- Chapter 2 - Meme Creator/src/js/meme.js
+++ Chapter 2 - Meme Creator/src/js/meme.js
@@ -4,8 +4,6 @@ class Meme {
       this.canvas = document.getElementById('imgCanvas');
       this.topText = document.getElementById('topText');
       this.bottomText = document.getElementById('bottomText');
-      this.canvasHeight = document.getElementById('canvasHeight');
-      this.canvasWidth = document.getElementById('canvasWidth');
       this.image = document.getElementById('image');
 
       this.createCanvas();
@@ -14,27 +12,71 @@ class Meme {
 
     createCanvas() {
       let deviceWidth = window.innerWidth;
-      let canvasWidth = Math.min(640, deviceWidth-30);
       let canvasHeight = Math.min(480, deviceWidth-30);
+      let canvasWidth = Math.min(640, deviceWidth-30);
 
-      this.canvasWidth.value = this.canvas.width = canvasWidth;
-      this.canvasHeight.value = this.canvas.height = canvasHeight;
+      this.canvas.height = canvasHeight;
+      this.canvas.width = canvasWidth;
 
-      // let ctx = this.canvas.getContext('2d');
-      // ctx.font = "30px Comic Sans MS";
-      // ctx.fillStyle = "red";
-      // ctx.textAlign = "center";
-      // ctx.fillText("Hello World This is totally awesome isn't it!!!", this.canvas.width/2, this.canvas.height/1.1);
-      // ctx.fillText("Hello World This is totally awesome isn't it!!!", this.canvas.width/2, this.canvas.height/5);
     }
 
     addEventListeners() {
-      [this.topText, this.bottomText, this.canvasHeight, this.canvasWidth].forEach(element => element.addEventListener('keyup', this.createMeme));
-      this.image.addEventListener('change', this.createMeme);
+      this.createMeme = this.createMeme.bind(this);
+      let inputNodes = [this.topText, this.bottomText, this.image];
+      inputNodes.forEach(element => element.addEventListener('keyup', this.createMeme));
+      inputNodes.forEach(element => element.addEventListener('change', this.createMeme));
+    }
+
+    resizeCanvas(canvasHeight, canvasWidth) {
+      let height = canvasHeight;
+      let width = canvasWidth;
+      this.canvas.style.height = `${height}px`;
+      this.canvas.style.width = `${width}px`;
+      while(height > 1000 || width > 1000) {
+        height /= 2;
+        width /= 2 ;
+        this.canvas.style.height = `${height}px`;
+        this.canvas.style.width = `${width}px`;
+      }
     }
 
     createMeme() {
-      console.log('create');
+      let ctx = this.canvas.getContext('2d');
+
+      if (this.image.files && this.image.files[0]) {
+        let reader = new FileReader();
+        let image = new Image();
+
+        reader.onload = event => {
+          image.onload = () => {
+
+            this.canvas.height = image.height;
+            this.canvas.width = image.width;
+            ctx.clearRect(0, 0, this.canvas.height, this.canvasWidth);
+            ctx.drawImage(image,0,0);
+
+            let fontSize = ((this.canvas.width+this.canvas.height)/2)*4/100;
+            ctx.lineWidth  = fontSize/5;
+            ctx.font = `${fontSize}pt sans-serif`;
+            ctx.strokeStyle = 'black';
+            ctx.fillStyle = 'white';
+            ctx.lineJoin = 'round';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'top';
+
+            ctx.strokeText(this.topText.value.toUpperCase(), this.canvas.width/2, this.canvas.height*(5/100));
+            ctx.fillText(this.topText.value.toUpperCase(), this.canvas.width/2, this.canvas.height*(5/100));
+
+            ctx.strokeText(this.bottomText.value.toUpperCase(), this.canvas.width/2, this.canvas.height*(90/100));
+            ctx.fillText(this.bottomText.value.toUpperCase(), this.canvas.width/2, this.canvas.height*(90/100));
+
+            this.resizeCanvas(this.canvas.height, this.canvas.width);
+          }
+          image.src = reader.result;
+        }
+
+        reader.readAsDataURL(this.image.files[0]);
+      }
     }
 }
 
