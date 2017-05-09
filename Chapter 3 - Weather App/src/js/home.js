66e04b611cd292ed3e94810a16b8206314b79a9b
DaniAkash <s.daniakash@gmail.com>
2017-05-09 07:48:32 +0530

    weather cover section UI

--- Chapter 3 - Weather App/src/js/home.js
+++ Chapter 3 - Weather App/src/js/home.js
@@ -4,3 +4,13 @@ window.addEventListener('WebComponentsReady', function(e) {
   // imports are loaded and elements have been registered
   console.log('Components are ready');
 });
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    console.log(startPos.coords.latitude);
+    console.log(startPos.coords.longitude);
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
