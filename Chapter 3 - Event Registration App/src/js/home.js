05e3ecec22316daa4d7f2a900bcdfca4f41d3eb3
DaniAkash <s.daniakash@gmail.com>
2017-05-26 00:23:56 +0530

    ERA - form validation

--- Chapter 3 - Event Registration App/src/js/home.js
+++ Chapter 3 - Event Registration App/src/js/home.js
@@ -1,7 +1,88 @@
 import './general';
+import validateRegistrationForm from './services/formValidation/registrationForm';
 
 class Home {
 
+  constructor() {
+    this.$form = document.querySelector('#registrationForm');
+    this.$username = document.querySelector('#username');
+    this.$email = document.querySelector('#email');
+    this.$phone = document.querySelector('#phone');
+    this.$age = document.querySelector('#age');
+    this.$profession = document.querySelector('#profession');
+    this.$experience = document.querySelector('#experience');
+    this.$comment = document.querySelector('#comment');
+
+    this.$form.addEventListener('submit', (event) => {
+      this.onFormSubmit(event);
+    });
+  }
+
+  getValues() {
+    return {
+      username: this.$username.value,
+      email: this.$email.value,
+      phone: this.$phone.value,
+      age: this.$age.value,
+      profession: this.$profession.value,
+      experience: this.$experience.value,
+      comment: this.$comment.value,
+    };
+  }
+
+  onFormSubmit() {
+    event.preventDefault();
+
+    const formValues = this.getValues();
+    const formStatus = validateRegistrationForm(formValues);
+
+    console.log(formStatus);
+
+    if(formStatus.isValid) {
+      this.clearErrors();
+      this.submitForm(formValues);
+    } else {
+      this.clearErrors();
+      this.highlightErrors(formStatus.result);
+    }
+  }
+
+  submitForm(formValues) {
+
+  }
+
+  highlightErrors(result) {
+    if(!result.username) {
+      this.$username.parentElement.classList.add('has-error');
+    }
+    if(!result.phone) {
+      this.$phone.parentElement.classList.add('has-error');
+    }
+    if(!result.email) {
+      this.$email.parentElement.classList.add('has-error');
+    }
+    if(!result.age) {
+      this.$age.parentElement.classList.add('has-error');
+    }
+    if(!result.profession) {
+      this.$profession.parentElement.classList.add('has-error');
+    }
+    if(!result.experience) {
+      this.$experience.parentElement.classList.add('has-error');
+    }
+  }
+
+  clearErrors() {
+      this.$username.parentElement.classList.remove('has-error');
+      this.$phone.parentElement.classList.remove('has-error');
+      this.$email.parentElement.classList.remove('has-error');
+      this.$age.parentElement.classList.remove('has-error');
+      this.$profession.parentElement.classList.remove('has-error');
+      this.$experience.parentElement.classList.remove('has-error');
+  }
+
 }
 
-new Home();
+window.addEventListener("load", () => {
+  new Home();
+});
---
+++ Chapter 3 - Event Registration App/src/js/services/formValidation/registrationForm.js
@@ -0,0 +1,48 @@
+export default function validateRegistrationForm(form) {
+
+  const result = {
+    username: validateUserName(form.username),
+    email: validateEmail(form.email),
+    phone: validatePhone(form.phone),
+    age: validateAge(form.age),
+    profession: validateProfession(form.profession),
+    experience: validateExperience(form.experience),
+  };
+
+  let each, isValid = true;
+  for(each in result) {
+    isValid = isValid && result[each];
+  }
+
+  return {
+    isValid,
+    result,
+  };
+
+}
+
+function validateUserName(name) {
+  return name.length > 3 ? true: false;
+}
+
+function validateEmail(email) {
+  var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return emailRegex.test(email);
+}
+
+function validatePhone(phone) {
+  var phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  return phoneRegex.test(phone);
+}
+
+function validateAge(age) {
+  return age >= 10 && age <= 25 ? true: false;
+}
+
+function validateProfession(value) {
+  return value ? true : false;
+}
+
+function validateExperience(experience) {
+  return experience > 0 && experience < 4 ? true : false;
+}
