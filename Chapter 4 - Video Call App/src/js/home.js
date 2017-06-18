80558ad522aa74321601034a7a8a23e4aadbe0dc
DaniAkash <s.daniakash@gmail.com>
2017-06-18 13:06:18 +0530

    add remote video and volume indicator

--- Chapter 4 - Video Call App/src/js/home.js
+++ Chapter 4 - Video Call App/src/js/home.js
@@ -17,33 +17,97 @@ let webrtc = new SimpleWebRTC({
     autoAdjustMic: false
 });
 
-$('form').submit(function () {
-    var val = $('#sessionInput').val().toLowerCase().replace(/\s/g, '-').replace(/[^A-Za-z0-9_\-]/g, '');
-    webrtc.createRoom(val, function (err, name) {
-        console.log(' create room cb', arguments);
-
-        var newUrl = location.pathname + '?' + name;
-        if (!err) {
-            history.replaceState({}, null, newUrl);
-            setRoom(name);
-        } else {
-            console.log(err);
-        }
-    });
-    return false;
-});
-
 let roomName;
 let $createRoomButton = document.querySelector('#createRoom');
 let $roomNameInput = document.querySelector('#roomName');
 $createRoomButton.onclick = () => {
   roomName = $roomNameInput.value.toLowerCase().replace(/\s/g, '-').replace(/[^A-Za-z0-9_\-]/g, '');
   if(roomName) {
-    let newUrl = location.pathname + '?' + roomName;
-    history.replaceState({foo: 'bar'}, null, newUrl);
-    webrtc.createRoom(roomName, function (err, name) {
-
+    webrtc.createRoom(roomName, (err, name) => {
+      if(!err) {
+        let newUrl = location.pathname + '?' + roomName;
+        history.replaceState({foo: 'bar'}, null, newUrl);
+      }
     });
   }
-
 };
+
+webrtc.on('readyToCall', function () {
+    const room = location.search && location.search.split('?')[1];
+    // you can name it anything
+    console.warn(room);
+    if (room) webrtc.joinRoom(room);
+});
+
+webrtc.on('videoAdded', ($video, peer) => {
+  const $remotes = document.querySelector('.remote-videos');
+
+  const $container = document.createElement('div');
+  $container.className = 'remote-video';
+  $container.id = 'container_' + webrtc.getDomId(peer);
+  $container.appendChild($video);
+
+  $video.className = 'remote-video-player';
+  // resize the video on click
+  $video.onclick = function () {
+      console.warn('clicked video', webrtc.getDomId(peer));
+      $container.style.width = $video.videoWidth + 'px';
+      $container.style.height = $video.videoHeight + 'px';
+  };
+
+  // show the remote volume
+  const $vol = document.createElement('meter');
+  $vol.id = 'volume_' + peer.id;
+  $vol.className = 'volume';
+  $vol.min = -45;
+  $vol.max = -20;
+  $vol.low = -40;
+  $vol.high = -25;
+  $container.appendChild($vol);
+
+  $remotes.appendChild($container);
+});
+
+const $buttonArea = document.querySelector('.room-text');
+const $copy = document.querySelector('.copy');
+const $copied = document.querySelector('.copied');
+
+$buttonArea.addEventListener('mouseenter', () => {
+  $copy.classList.remove('hidden');
+});
+
+$copy.addEventListener('click', () => {
+  $copy.classList.add('hidden');
+  $copied.classList.remove('hidden');
+});
+
+$copied.addEventListener('click', () => {
+  $copied.classList.remove('hidden');
+});
+
+$buttonArea.addEventListener('mouseout', (event) => {
+  var e = event.toElement || event.relatedTarget;
+  if(e) {
+    if (e.parentNode == $buttonArea || e == $buttonArea) {
+      return;
+    }
+  }
+  $copy.classList.add('hidden');
+  $copied.classList.add('hidden');
+});
+
+function showVolume(el, volume) {
+    if (!el) return;
+    if (volume < -45) volume = -45; // -45 to -20 is
+    if (volume > -20) volume = -20; // a good range
+    el.value = volume;
+}
+
+// local volume has changed
+webrtc.on('volumeChange', function (volume, treshold) {
+    showVolume(document.getElementById('localVolume'), volume);
+});
+// remote volume has changed
+webrtc.on('remoteVolumeChange', function (peer, volume) {
+    showVolume(document.getElementById('volume_' + peer.id), volume);
+});
