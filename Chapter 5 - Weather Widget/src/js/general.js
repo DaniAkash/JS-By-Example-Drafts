edbef866cd110b44b0bb2a8a67c7973d3f16ea16
DaniAkash <s.daniakash@gmail.com>
2017-07-09 21:51:56 +0530

    create weather widget project

---
+++ Chapter 5 - Weather Widget/src/js/general.js
@@ -0,0 +1,20 @@
+/**
+ * CSS files
+ */
+import "../../node_modules/bootstrap/less/bootstrap.less";
+import '../css/styles.css';
+
+/**
+ * Bootstrap JS File
+ */
+import 'bootstrap';
+
+/**
+ * Web Component Polyfill
+ */
+import 'webcomponents.js';
+
+/**
+ * Skycons
+ */
+// import './skycons';
