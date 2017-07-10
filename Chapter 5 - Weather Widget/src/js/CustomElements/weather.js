f7da3eff70eb799b8369adca5cbf0591901081c9
DaniAkash <s.daniakash@gmail.com>
2017-07-11 01:22:39 +0530

    create weather element

---
+++ Chapter 5 - Weather Widget/src/js/CustomElements/weather.js
@@ -0,0 +1,72 @@
+import '../skycons';
+
+export default class weather extends HTMLElement {
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({mode: 'open'});
+  }
+
+  connectedCallback() {
+    this._shadowRoot.innerHTML = `
+      <style>
+        .weather-container {
+          height: 100%;
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          align-items: center;
+          background-color: silver;
+          justify-content: space-between;
+        }
+        .title {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        .details {
+          flex: 2;
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+          align-items: center;
+          justify-content: center;
+        }
+        .day-icon {
+          flex: 1;
+          max-height: 70%;
+          max-width: 70%;
+        }
+        .text {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+        }
+        .text-content {
+          margin: 0px;
+        }
+      </style>
+      <div class="weather-container">
+        <div class="title">
+          <h2>Chennai, CH</h2>
+        </div>
+        <div class="details">
+          <canvas id="dayIcon" class="day-icon"></canvas>
+          <div class="text">
+            <h2 class="text-content">32 C</h2>
+            <p class="text-content">03:04 pm</p>
+            <p class="text-content">Drizzle</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    let skycons = new Skycons({"color": "black"});
+    skycons.add(this._shadowRoot.querySelector('#dayIcon'), Skycons.RAIN);
+    skycons.play();
+  }
+}
--- Chapter 5 - Weather Widget/src/js/home.js
+++ Chapter 5 - Weather Widget/src/js/home.js
@@ -1,2 +1,7 @@
 import './general';
 
+import Weather from './CustomElements/weather';
+
+window.addEventListener('WebComponentsReady', function(e) {
+  window.customElements.define('x-weather', Weather);
+});
