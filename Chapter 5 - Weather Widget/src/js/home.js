1808faea92e5697a99d941882ed8a66712915f41
DaniAkash <s.daniakash@gmail.com>
2017-07-12 07:30:26 +0530

    get Geo Location

--- Chapter 5 - Weather Widget/src/js/home.js
+++ Chapter 5 - Weather Widget/src/js/home.js
@@ -5,3 +5,23 @@ import Weather from './CustomElements/weather';
 window.addEventListener('WebComponentsReady', function(e) {
   window.customElements.define('x-weather', Weather);
 });
+
+window.onload = function() {
+  getLocation();
+};
+
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, errorPosition);
+  } else {
+      console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  console.log(position.coords.latitude, position.coords.longitude);
+}
+
+function errorPosition(error) {
+  console.log(error, 'unable to get position');
+}
