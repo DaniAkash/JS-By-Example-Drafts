04431f660ec6f43f0722375a01560cafca9d4f69
DaniAkash <s.daniakash@gmail.com>
2017-07-13 20:59:08 +0530

    set weather details in response

--- Chapter 5 - Proxy Server/app.js
+++ Chapter 5 - Proxy Server/app.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express'),
   bodyParser = require('body-parser'),
   DarkSky = require('dark-sky'),
-  forecast = new DarkSky(process.env.API_KEY);
+  forecast = new DarkSky(process.env.DARK_SKY_KEY);
 
 const app = express();
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -22,16 +22,16 @@ app.get('/getWeather/:latlong', (req, res, next) => {
       .longitude(latlong[1])
       .exclude('minutely,hourly,daily,alerts,flags')
       .get()
-      .then(res => {
-        console.log(res);
+      .then(response => {
+        res.status(200).json(response);
       })
       .catch(err => {
-        console.error(err);
+        res.status(500).json({});
       });
   }
 });
 
 const http = require('http').Server(app);
-http.listen(app.get('port'), function() {
+http.listen(app.get('port'), () => {
   console.log(`Express Server Listening on port ${app.get('port')}.`);
 });
