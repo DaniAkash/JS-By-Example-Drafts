b81569b19ae4cb2cef66af9409ef0435d1070153
DaniAkash <s.daniakash@gmail.com>
2017-07-30 19:08:47 +0530

    add comment to a post

---
+++ api/controllers/comment.js
@@ -0,0 +1,37 @@
+module.exports = {
+  addComment,
+};
+
+const db = require('../helpers/db').db;
+const _ = require('lodash');
+
+function addComment(req, res) {
+
+    const allPosts = db.get('posts').value();
+
+    const requiredPost = db.get('posts')
+        .find({id: req.body.postId})
+        .value();
+
+    requiredPost.comments = [{
+        id: req.body.id,
+        name: req.body.name,
+        comment: req.body.comment,
+    }, ...requiredPost.comments]
+
+    const newPosts = allPosts.map(post => {
+        if(post.id === requiredPost.id) return requiredPost;
+        return post;
+    });
+
+    db
+    .set('posts', newPosts)
+    .write()
+    .then(() => {
+        res.status(200).json({message: 'Comment added successfully'});
+    })
+    .catch(() => {
+        res.status(500).json({message: 'Unable to add comment'});
+    });
+
+}
