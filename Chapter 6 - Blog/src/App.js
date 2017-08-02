aaf6e7644714d97db79d05deec9ac0fa6041c39d
DaniAkash <s.daniakash@gmail.com>
2017-08-03 05:03:31 +0530

    create home

--- Chapter 6 - Blog/src/App.js
+++ Chapter 6 - Blog/src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink, Route } from 'react-router-dom';
+
 import './App.css';
+import routes from './routes';
+import Home from './Components/Home/Home';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavLink className={'navbar-brand'} to={routes.home}>Home</NavLink>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to={routes.home}>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to={routes.authors}>Authors</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className={'nav-link'} activeClassName={'active'} to={routes.newPost}>New Post</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <Route exact path={routes.home} component={Home} />
       </div>
     );
   }
