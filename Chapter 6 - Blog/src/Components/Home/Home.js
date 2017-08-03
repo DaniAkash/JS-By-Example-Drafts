a0a90e491567d200659645bfcd7c942ab2ca225d
DaniAkash <s.daniakash@gmail.com>
2017-08-03 06:25:25 +0530

    get posts

--- Chapter 6 - Blog/src/Components/Home/Home.js
+++ Chapter 6 - Blog/src/Components/Home/Home.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import apiCall from '../../services/api/apiCall';
 
 class Home extends Component {
 
   constructor() {
     super();
+
+    this.state = {
+      posts: [],
+      loading: true,
+      hasError: false,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({loading: true});
+    apiCall('posts', {}, 'GET')
+    .then(allPosts => {
+      const posts = allPosts.posts;
+      this.setState({posts, loading: false});
+    })
+    .catch(error => {
+      this.setState({hasError: true, loading: false});
+      console.error(error);
+    });
   }
 
   render () {
