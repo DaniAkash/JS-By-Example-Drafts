082983a7c9fe6f4820fd8042f10b4d2ddaa74353
DaniAkash <s.daniakash@gmail.com>
2017-08-05 20:34:06 +0530

    create author and posts actions with thunk

---
+++ Chapter 7 - Redux/src/redux/actions/postsActions.js
@@ -0,0 +1,43 @@
+import actions from '../actionTypes/actionTypes';
+import apiCall from '../../services/api/apiCall';
+
+export const getAllPosts = dispatch => {
+
+  dispatch(postsApiCallStart());
+
+  apiCall('posts', {}, 'GET')
+    .then(posts => {
+      dispatch(postsApiCallSuccess());
+      dispatch(getPosts(posts));
+    })
+    .catch(error => {
+      dispatch(postsApiCallFailure());
+      console.error(error);
+    });
+
+};
+
+export const postsApiCallStart = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_START,
+  };
+};
+
+export const postsApiCallSuccess = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_SUCCESS,
+  };
+};
+
+export const postsApiCallFailure = () => {
+  return {
+    type: actions.GET_POSTS_AJAX_CALL_FAILURE,
+  };
+};
+
+export const getPosts = (posts) => {
+  return {
+    type: actions.GET_POSTS,
+    data: posts,
+  };
+};
