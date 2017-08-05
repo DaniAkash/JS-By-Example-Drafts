bf154bc4cb48a23ab2a7684c42f331e677e756c8
DaniAkash <s.daniakash@gmail.com>
2017-08-05 22:37:43 +0530

    create author and posts reducers

--- Chapter 7 - Redux/src/redux/reducers/authorsReducer.js
+++ Chapter 7 - Redux/src/redux/reducers/authorsReducer.js
@@ -1,6 +1,11 @@
 import initialState from '../store/initialState';
+import actions from '../actionTypes/actionTypes';
 
 const authorsReducer = (state = initialState.authors, action) => {
+  switch(action.type) {
+    case actions.GET_AUTHORS:
+      return action.data;
+  }
 
   return state;
 };
--- Chapter 7 - Redux/src/redux/reducers/postsReducer.js
+++ Chapter 7 - Redux/src/redux/reducers/postsReducer.js
@@ -1,6 +1,11 @@
 import initialState from '../store/initialState';
+import actions from '../actionTypes/actionTypes';
 
 const postsReducer = (state = initialState.posts, action) => {
+  switch(action.type) {
+    case actions.GET_POSTS:
+      return action.data;
+  }
 
   return state;
 };
