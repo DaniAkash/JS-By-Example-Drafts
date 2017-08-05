6edb622098c0676db072bc2815374dd7ca715884
DaniAkash <s.daniakash@gmail.com>
2017-08-05 10:54:52 +0530

    remove unwanted import

--- Chapter 6 - Blog/src/Components/Author/AuthorPosts.js
+++ Chapter 6 - Blog/src/Components/Author/AuthorPosts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import apiCall from '../../services/api/apiCall';
