f899031f14ac48e55c750ce2ec32518d1d50e3ef
DaniAkash <s.daniakash@gmail.com>
2017-08-05 16:51:10 +0530

    create Chapter 7

---
+++ Chapter 7 - Redux/src/Components/Post/Post.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import ErrorMessage from '../Common/ErrorMessage';
+import apiCall from '../../services/api/apiCall';
+import LoadingIndicator from '../Common/LoadingIndicator';
+
+class Post extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      post: {},
+      loading: true,
+      hasError: false,
+    };
+  }
+
+  componentWillMount() {
+    this.setState({loading: true});
+    apiCall(`post/${this.props.match.params.id}`, {}, 'GET')
+    .then(post => {
+      this.setState({post, loading: false});
+    })
+    .catch(error => {
+      this.setState({hasError: true, loading: false});
+      console.error(error);
+    });
+  }
+
+  render() {
+    return(
+      <div className={`post-container container`}>
+        {
+          this.state.loading
+          ?
+            <LoadingIndicator />
+          :
+            null
+        }
+        {
+          this.state.hasError
+          ?
+            <ErrorMessage title={'Error!'} message={`Unable to retrieve post!`} />
+          :
+            null
+        }
+        <h2>{this.state.post.title}</h2>
+        <p>{this.state.post.author}</p>
+        <p>{this.state.post.content}</p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Post);
