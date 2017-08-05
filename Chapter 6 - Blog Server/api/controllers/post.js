d3865969965780bf620936bdc8fde3e8c873a18f
DaniAkash <s.daniakash@gmail.com>
2017-08-05 15:01:49 +0530

    set timeout while adding posts

--- Chapter 6 - Blog Server/api/controllers/post.js
+++ Chapter 6 - Blog Server/api/controllers/post.js
@@ -13,7 +13,7 @@ function addPost(req, res) {
           .write()
           .then(() => res.status(200).json({message: 'Post added Successfully'}))
           .catch(error => res.status(500).json({message: 'Unable to add post!'}));
-    });
+    }, 3000);
 }
 
 function post(req, res) {
