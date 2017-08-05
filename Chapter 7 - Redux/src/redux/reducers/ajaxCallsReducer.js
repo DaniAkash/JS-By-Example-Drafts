e15d78363b34df3b9fc824d1f3b310cefe8468a6
DaniAkash <s.daniakash@gmail.com>
2017-08-05 19:44:39 +0530

    ajax calls reducer

---
+++ Chapter 7 - Redux/src/redux/reducers/ajaxCallsReducer.js
@@ -0,0 +1,8 @@
+import initialState from '../store/initialState';
+
+const ajaxCallsReducer = (state = initialState.ajaxCalls, action) => {
+
+  return state;
+};
+
+export default ajaxCallsReducer;
--- Chapter 7 - Redux/src/redux/store/initialState.js
+++ Chapter 7 - Redux/src/redux/store/initialState.js
@@ -5,6 +5,20 @@ const initialState = {
   authors: [
 
   ],
+  ajaxCalls: {
+    getAllPosts: {
+      isLoading: false,
+      hasError: false,
+    },
+    getAuthors: {
+      isLoading: false,
+      hasError: false,
+    },
+    addPost: {
+      isLoading: false,
+      hasError: false,
+    }
+  }
 };
 
 export default initialState;
